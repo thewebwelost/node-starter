f1acfc1b0e40565d5837c798b9d044a88b0fda65
thewebwelost <thawebwelost@gmail.com>
2022-07-29 19:40:10 -0700

    minor controllers cleanup and fix

--- controllers/dudesController.js
+++ controllers/dudesController.js
@@ -1,25 +1,28 @@
 const Dude = require('../model/Dude');
 
 const getAllDudes = async (req, res) => {
-  const dudes = await Dude.find({});
+  const dudes = await Dude.find();
+  if (!dudes) return res.status(204).json({ message: 'No dudes!' });
   res.json(dudes);
 };
 
 const addDude = async (req, res) => {
   const { firstname, lastname, copy } = req.body;
-
   if (!firstname || !lastname)
     return res
       .status(400)
       .json({ message: "Man, I don't even know your name!" });
 
-  const newDude = await Dude.create({
-    firstname,
-    lastname,
-    copy,
-  });
-
-  res.status(201).json(newDude);
+  try {
+    const newDude = await Dude.create({
+      firstname,
+      lastname,
+      copy,
+    });
+    res.status(201).json(newDude);
+  } catch (err) {
+    console.err(err);
+  }
 };
 
 const updateDude = async (req, res) => {
@@ -30,19 +33,22 @@ const updateDude = async (req, res) => {
       .json({ message: "You've gotta give me his numbers man" });
   }
 
-  const myDude = await Dude.findByIdAndUpdate(
-    id,
-    {
-      firstname,
-      lastname,
-      copy,
-    },
-    {},
-    (err, data) =>
-      err ? console.log(err) : console.log('Updated User : ', data)
-  );
-
-  res.status(201).json(myDude);
+  try {
+    const myDude = await Dude.findByIdAndUpdate(
+      id,
+      {
+        firstname,
+        lastname,
+        copy,
+      },
+      {},
+      (err, data) =>
+        err ? console.log(err) : console.log('Updated User : ', data)
+    );
+    res.status(201).json(myDude);
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 const deleteDude = async (req, res) => {
@@ -53,27 +59,30 @@ const deleteDude = async (req, res) => {
       .json({ message: "You've gotta give me his numbers man" });
   }
 
-  await Dude.findByIdAndDelete(id, {}, (err, data) =>
-    err ? console.log(err) : console.log('Updated User : ', data)
-  );
-
-  res.status(201).json(data.dudes);
+  try {
+    await Dude.findByIdAndDelete(id);
+    res.sendStatus(204);
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 const getDude = async (req, res) => {
-  if (!parseInt(req.params.id)) {
+  if (!req.params.id) {
     return res
       .status(400)
       .json({ message: "You've gotta give me his numbers man" });
   }
 
-  const myDude = await Dude.findById(parseInt(req.params.id));
-
-  if (!myDude) {
-    return res.status(400).json({ message: "I don't even know the dude!" });
+  try {
+    const myDude = await Dude.findById(req.params.id);
+    if (!myDude) {
+      return res.status(400).json({ message: "I don't even know the dude!" });
+    }
+    return res.status(200).json(myDude);
+  } catch (err) {
+    console.error(err);
   }
-
-  return res.json(myDude);
 };
 
 module.exports = {
